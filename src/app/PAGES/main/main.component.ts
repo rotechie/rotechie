a2b1fd2ec1bc522377734abd985464dd549c4207
eneaantonicelli <eneaantonicelli@gmail.com>
2019-11-30 23:25:57 -0800

    Added beginning of the main page mouseevent. Not working as of right now

--- src/app/PAGES/main/main.component.ts
+++ src/app/PAGES/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, Renderer2 } from "@angular/core";
 
 @Component({
   selector: "app-main",
@@ -6,7 +6,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
-  constructor() {}
+  globalListenFunc: Function;
 
-  ngOnInit() {}
+  constructor(
+    private elementVisible: ElementRef,
+    private professional: ElementRef,
+    private leisure: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.elementVisible.nativeElement = document.querySelectorAll(
+      ".ega_backgroundimage"
+    );
+    this.professional.nativeElement = document.querySelectorAll(
+      ".ega_logoman_image .left"
+    );
+    this.leisure.nativeElement = document.querySelectorAll(
+      ".ega_logoman_image .right"
+    );
+    console.log(this.elementVisible.nativeElement);
+    this.globalListenFunc = this.renderer.listen("document", "mousemove", e => {
+      let imageTop = this.elementVisible.nativeElement.getBoundingClientRect()
+        .top;
+      let imageLeft = this.elementVisible.nativeElement.getBoundingClientRect()
+        .left;
+      let imageBottom = imageTop + this.elementVisible.nativeElement.height();
+      let imageRight = imageLeft + this.elementVisible.nativeElement.width();
+      var mouseX = e.pageX;
+      var mouseY = e.pageY;
+      if (mouseY < imageTop || mouseY > imageBottom) {
+        return;
+      }
+      console.log(imageTop);
+      this.elementVisible.nativeElement.hide();
+      if (mouseX > imageLeft && mouseX < imageRight) {
+        this.leisure.nativeElement.show();
+      } else if (mouseX < imageLeft) {
+        this.professional.nativeElement.show();
+      } else {
+        this.leisure.nativeElement.show();
+      }
+    });
+  }
+  ngOnDestroy() {
+    // remove listener
+    this.globalListenFunc();
+  }
 }
