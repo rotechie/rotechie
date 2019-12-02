93f5f873f18934e657df9906841ab57dfecb528d
eneaantonicelli <eneaantonicelli@gmail.com>
2019-12-02 12:31:18 -0800

    fixed the changing main image

--- src/app/PAGES/main/main.component.ts
+++ src/app/PAGES/main/main.component.ts
@@ -9,43 +9,45 @@ export class MainComponent implements OnInit {
   globalListenFunc: Function;
 
   constructor(
-    private elementVisible: ElementRef,
     private professional: ElementRef,
     private leisure: ElementRef,
     private renderer: Renderer2
   ) {}
 
   ngOnInit() {
-    this.elementVisible.nativeElement = document.querySelectorAll(
-      ".ega_backgroundimage"
+    this.professional.nativeElement = document.querySelector(
+      ".ega_logoman_image_left"
     );
-    this.professional.nativeElement = document.querySelectorAll(
-      ".ega_logoman_image .left"
+    this.leisure.nativeElement = document.querySelector(
+      ".ega_logoman_image_right"
     );
-    this.leisure.nativeElement = document.querySelectorAll(
-      ".ega_logoman_image .right"
-    );
-    console.log(this.elementVisible.nativeElement);
+
+    let professional = this.professional.nativeElement;
+    let leisure = this.leisure.nativeElement;
+    professional.hidden = true;
+    leisure.hidden = false;
+
+    let getWidth = function() {
+      return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+      );
+    };
+    getWidth();
+    var getWidthDivision = getWidth() / 2;
+
     this.globalListenFunc = this.renderer.listen("document", "mousemove", e => {
-      let imageTop = this.elementVisible.nativeElement.getBoundingClientRect()
-        .top;
-      let imageLeft = this.elementVisible.nativeElement.getBoundingClientRect()
-        .left;
-      let imageBottom = imageTop + this.elementVisible.nativeElement.height();
-      let imageRight = imageLeft + this.elementVisible.nativeElement.width();
       var mouseX = e.pageX;
-      var mouseY = e.pageY;
-      if (mouseY < imageTop || mouseY > imageBottom) {
-        return;
-      }
-      console.log(imageTop);
-      this.elementVisible.nativeElement.hide();
-      if (mouseX > imageLeft && mouseX < imageRight) {
-        this.leisure.nativeElement.show();
-      } else if (mouseX < imageLeft) {
-        this.professional.nativeElement.show();
+
+      if (mouseX < getWidthDivision) {
+        professional.hidden = false;
+        leisure.hidden = true;
       } else {
-        this.leisure.nativeElement.show();
+        leisure.hidden = false;
+        professional.hidden = true;
       }
     });
   }
